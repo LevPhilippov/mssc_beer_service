d49932d91d3c67d86889910ab8ec1608e4b8da1e
LevPhilippov <filippov.l.a.89@gmail.com>
2021-12-09 19:09:03 +0700

    Created BeerControllerTests with simple testing methods. Fix #3

---
+++ src/test/java/lev/philippov/mssc_beer_service/web/controllers/BeerControllerTest.java
@@ -0,0 +1,50 @@
+package lev.philippov.mssc_beer_service.web.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lev.philippov.mssc_beer_service.web.models.BeerDto;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    private final static String API_V1_URI = "/api/v1/beer";
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Test
+    void getBeerById() throws Exception {
+        mockMvc.perform(get(API_V1_URI+"/"+ UUID.randomUUID()))
+                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void createNewBeer() throws Exception {
+        String jsonBeerDto = mapper.writeValueAsString(BeerDto.builder().build());
+        mockMvc.perform(post(API_V1_URI).contentType(MediaType.APPLICATION_JSON).content(jsonBeerDto)).andExpect(status().isNoContent());
+    }
+
+    @Test
+    void updateBeerById() throws Exception {
+        String jsonBeerDto = mapper.writeValueAsString(BeerDto.builder().build());
+        mockMvc.perform(put(API_V1_URI + "/" + UUID.randomUUID())
+                .contentType(MediaType.APPLICATION_JSON).content(jsonBeerDto))
+                .andExpect(status().isNoContent());
+    }
+}
