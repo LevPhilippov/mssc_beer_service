9ce183f66aa9782df1bc9b5473cc1a4d2e2ae2c2
LevPhilippov <filippov.l.a.89@gmail.com>
2021-12-10 16:49:51 +0700

    Created JPA Entity fo Beer. Fix #4

---
+++ src/main/java/lev/philippov/mssc_beer_service/domain/Beer.java
@@ -0,0 +1,39 @@
+package lev.philippov.mssc_beer_service.domain;
+
+import lev.philippov.mssc_beer_service.web.models.BeerStyleEnum;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id",length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+    private Integer quantityOnHands;
+    private BigDecimal price;
+    @Version
+    private Integer version;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private OffsetDateTime createdDate;
+    @UpdateTimestamp
+    private OffsetDateTime lastModifiedDate;
+}
