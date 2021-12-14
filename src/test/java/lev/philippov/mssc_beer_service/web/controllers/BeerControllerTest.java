352c1b943cf8b1885a39b243099d226089da7050
LevPhilippov <filippov.l.a.89@gmail.com>
2021-12-14 16:33:38 +0700

    Added RestDocs support.

--- src/test/java/lev/philippov/mssc_beer_service/web/controllers/BeerControllerTest.java
+++ src/test/java/lev/philippov/mssc_beer_service/web/controllers/BeerControllerTest.java
@@ -2,22 +2,37 @@ package lev.philippov.mssc_beer_service.web.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lev.philippov.mssc_beer_service.domain.Beer;
 import lev.philippov.mssc_beer_service.web.models.BeerDto;
 import lev.philippov.mssc_beer_service.web.models.BeerStyleEnum;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureRestDocs
+@ExtendWith(RestDocumentationExtension.class)
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
@@ -31,15 +46,40 @@ class BeerControllerTest {
 
     @Test
     void getBeerById() throws Exception {
-        mockMvc.perform(get(API_V1_URI+"/"+ UUID.randomUUID()))
+        mockMvc.perform(get(API_V1_URI+"/{beerId}",UUID.randomUUID()))
                 .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(document("v1/beer-get",
+                        pathParameters(parameterWithName("beerId").description("UUID of desired beer to get")),
+                        responseFields(fieldWithPath("id").description("Id of a Beer"),
+                                       fieldWithPath("beerName").description("Name of a Beer, Not Null"),
+                                       fieldWithPath("beerStyle").description("Style of a Beer, Not Null"),
+                                       fieldWithPath("upc").description("Upc of a Beer, Not Null, positive"),
+                                       fieldWithPath("quantityOnHands").description("Quantity on hands, nullable"),
+                                       fieldWithPath("price").description("Price of a Beer, Not Null, positive"),
+                                       fieldWithPath("version").description("Version"),
+                                       fieldWithPath("createdDate").description("Creation timestamp"),
+                                       fieldWithPath("lastModifiedDate").description("Modification timestamp")))
+                );
     }
 
     @Test
     void createNewBeer() throws Exception {
         String jsonBeerDto = mapper.writeValueAsString(getValidBeerDto());
-        mockMvc.perform(post(API_V1_URI).contentType(MediaType.APPLICATION_JSON).content(jsonBeerDto)).andExpect(status().isNoContent());
+        ConstrainedFields fields = new ConstrainedFields(Beer.class);
+        mockMvc.perform(post(API_V1_URI).contentType(MediaType.APPLICATION_JSON).content(jsonBeerDto))
+                .andExpect(status().isNoContent())
+                .andDo(document("v1/beer-new",
+                        requestFields(fieldWithPath("id").ignored(),
+                                fields.withPath("beerName").description("Name of a Beer, Not Null"),
+                                fields.withPath("beerStyle").description("Style of a Beer, Not Null"),
+                                fields.withPath("upc").description("Upc of a Beer, Not Null, positive"),
+                                fields.withPath("quantityOnHands").ignored(),
+                                fields.withPath("price").description("Price of a Beer, Not Null, positive"),
+                                fields.withPath("version").ignored(),
+                                fields.withPath("createdDate").ignored(),
+                                fields.withPath("lastModifiedDate").ignored()
+                        )));
     }
 
     @Test
@@ -52,5 +92,20 @@ class BeerControllerTest {
 
     private BeerDto getValidBeerDto(){
         return BeerDto.builder().beerName("My Beer").beerStyle(BeerStyleEnum.IPA).price(new BigDecimal("12")).upc(123123123123L).build();
+    }
+
+    private static class ConstrainedFields {
+
+        private final ConstraintDescriptions constraintDescriptions;
+
+        ConstrainedFields(Class<?> input) {
+            this.constraintDescriptions = new ConstraintDescriptions(input);
+        }
+
+        private FieldDescriptor withPath(String path) {
+            return fieldWithPath(path).attributes(key("constraints").value(StringUtils
+                    .collectionToDelimitedString(this.constraintDescriptions
+                            .descriptionsForProperty(path), ". ")));
+        }
     }
 }
