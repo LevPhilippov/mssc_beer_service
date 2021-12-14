dbfc76de05b23d16ba6ff8f25228c6e72188567b
LevPhilippov <filippov.l.a.89@gmail.com>
2021-12-14 19:24:12 +0700

    Added Jackson Creator example. Closes #13

--- src/main/java/lev/philippov/mssc_beer_service/web/models/BeerDtoPage.java
+++ src/main/java/lev/philippov/mssc_beer_service/web/models/BeerDtoPage.java
@@ -1,11 +1,30 @@
 package lev.philippov.mssc_beer_service.web.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public class BeerDtoPage extends PageImpl<BeerDto> {
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BeerDtoPage(@JsonProperty("totalPages") int totalPages,
+                       @JsonProperty("totalElements") long totalElements,
+                       @JsonProperty("last") boolean last,
+                       @JsonProperty("number") int number,
+                       @JsonProperty("size") int size,
+                       @JsonProperty("numberOfElements") int numberOfElements,
+                       @JsonProperty("first") boolean first,
+                       @JsonProperty("content") List<BeerDto> content,
+                       @JsonProperty("sort") JsonNode sort,
+                       @JsonProperty("pageable") JsonNode pageable){
+        super(content, PageRequest.of(number,size),totalElements);
+    }
+
 
     public BeerDtoPage(List<BeerDto> content, Pageable pageable, long total) {
         super(content, pageable, total);
